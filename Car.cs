d734d1f52d2a109273257458fde52b0d8c0d914a
Poul Henriksen <pohe@easj.dk>
2019-11-12 10:57:32 +0100

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonTest
+{
+    public class Car
+    {
+        //static instance til at refere til objektet
+        private static Car _instance = new Car(); //Eager initialization
+
+        //Static property til at tilgå objektet
+        public static Car Instance
+        {
+            get
+            {   // Lazy initialization
+                //if (_instance == null)
+                //{
+                //    _instance = new Car();
+                //}
+                return _instance;
+            }
+           
+        }
+
+        public string Model { get; set; }
+        public string Regnr { get; set; }
+
+
+        //Constructor er private
+        private Car()
+        {
+        }
+
+        public override string ToString()
+        {
+            return Model + " " + Regnr;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Car car1 = new Car();
+            //car1.Model = "VW";
+            //car1.Regnr = "AB 37 571";
+
+            //Car car2 = new Car();
+            //car2.Model = "Skoda";
+            //car2.Regnr = "FG 34344";
+
+            Car car1 = Car.Instance;
+            car1.Model = "VW";
+            car1.Regnr = "AB 37 571";
+
+            Car car2 = Car.Instance;
+            Console.WriteLine("Car1 " +  car1.ToString());
+            Console.WriteLine("Car2 " + car2.ToString());
+
+            car2.Model = "Skoda";
+            Console.WriteLine("Car1 " + car1.ToString());
+            Console.WriteLine("Car2 " + car2.ToString());
+
+
+            Console.ReadLine();
+        }
+    }
+}
